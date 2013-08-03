390459dbc46ffdee98785d6ad3775a1880bdb45d
maochangming <maochangming@YVZSSFZWOC5E33E>
2013-08-03 14:49:19 +0800

    在工程中删除derby数据库，并将数据库名称修改为auctiondb

--- resource/script.sql
+++ resource/script.sql
@@ -1,3 +1,9 @@
+重要说明：
+运行ij.bat进行ij，进行命令交互
+首次建立新库，并使用hrxc用户名：connect 'jdbc:derby://localhost:1527/auctiondb;user=hrxc;create=true';
+建立连接：connect 'jdbc:derby://localhost:1527/auctiondb;user=hrxc';
+服务启动后，如果通过网络无法联通derby数据库，则需要将start...Server.bat与stop...Server.bat中localhost修改为物理实际IP地址，然后再启动；
+
 CREATE TABLE HRXC.BARGAIN_RECORD  ( 
     PK_ID             	VARCHAR(32) NOT NULL,
     PADDLE_NO         	VARCHAR(20),
